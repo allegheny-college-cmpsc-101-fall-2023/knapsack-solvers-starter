59510c995d259d49c3f3f6f6f1b7cc420e30d658
Emily Graber <egragraber@allegheny.edu>
2023-11-29 17:42:33 -0500

    Update README.md

--- source/demonstrate_knapsack_solvers.py
+++ source/demonstrate_knapsack_solvers.py
@@ -44,14 +44,8 @@ def greedy(items: List[Item], max_weight: int, key_function) -> Tuple[List[Item]
     # TODO: Add this source code segment by consulting the book and course web site
 
 
-def build_items() -> List[Item]:
+def build_items(n: List[str], v: List[int], w: List[int]) -> List[Item]:
     """Create an instance of a 0/1 knapsack using instances of the Item class."""
-    # NOTE: You are welcome to try the program with a different instance of the
-    # 0/1 Knapsack solver than the one provided in the book. However, make sure that when
-    # you have your final submission you are using the same example as in Figure 14-1.
-    names = ["Clock", "Painting", "Radio", "Vase", "Book", "Computer"]
-    values = [175, 90, 20, 50, 10, 200]
-    weights = [10, 9, 4, 2, 1, 20]
     items: List[Item] = []
     for i in range(len(values)):
         items.append(Item(names[i], values[i], weights[i]))
@@ -70,24 +64,31 @@ def powerset(s: List[Item]):
 
 def exhaustive_enumeration(pset, max_weight, get_value, get_weight):
     """Run an exhaustive enumeration algorithm to find best combination."""
-    # TODO: Provide an implementation of this function by consulting the book and course web site
+    # TODO: Provide an implementation of this function by consulting the book and course website
 
 
-def run_exhaustive_enumeration(max_weight=20):
+def run_exhaustive_enumeration(items: List[Items], max_weight=20):
     """Use the exhaustive enumeration algorithm for a problem instance."""
-    # TODO: Provide an implementation of this function by consulting the book and course web site
+    # TODO: Provide an implementation of this function by consulting the book and course website
 
 
-def run_greedy(items: List[Item], max_weight: int, key_function) -> None:
+def run_one_greedy(items: List[Item], max_weight: int, key_function) -> None:
     """Run the greedy algorithm and display the result."""
-    # TODO: Provide an implementation of this function by consulting the book and course web site
+    # TODO: Provide an implementation of this function by consulting the book and course website
 
 
-def run_all_greedy(max_weight=20) -> None:
+def run_all_greedy(items: List[Item], max_weight=20) -> None:
     """Run all greedy algorithm with all possible objective functions."""
-    # TODO: Provide an implementation of this function by consulting the book and course web site
+    # TODO: Provide an implementation of this function by consulting the book and course website
 
 
 if __name__ == "__main__":
+    # NOTE: You are welcome to try the program with a different instance of the
+    # 0/1 Knapsack solver than the one provided in the book. However, make sure that when
+    # you have your final submission you are using the same example as in Figure 14-1.
+    names = ["Clock", "Painting", "Radio", "Vase", "Book", "Computer"]
+    values = [175, 90, 20, 50, 10, 200]
+    weights = [10, 9, 4, 2, 1, 20]
+    # TODO: build the items structure
     # TODO: run the greedy algorithm and display its output
     # TODO: run the exhaustive enumeration algorithm and display its output
